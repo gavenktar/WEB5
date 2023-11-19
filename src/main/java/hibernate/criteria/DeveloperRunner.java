2f824aed9e84321172165fb62f0a57cc3e86184c
Kiryl Arol <kiry.arol@gmail.com>
2023-11-19 20:30:29 +0300

    copypaste part 2

---
+++ src/main/java/hibernate/criteria/DeveloperRunner.java
@@ -0,0 +1,90 @@
+package hibernate.criteria;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+public class DeveloperRunner {
+    private static SessionFactory sessionFactory;
+
+    public static void main(String[] args) {
+
+
+        Configuration configuration = new Configuration().configure();
+        configuration.setProperty("hibernate.current_session_context_class", "thread");
+
+        configuration.setProperty("hibernate.hbm2ddl.auto", "update");
+
+        sessionFactory = configuration.buildSessionFactory();
+
+        DeveloperRunner developerRunner = new DeveloperRunner();
+
+        System.out.println("Adding developer's records to the database...");
+        Integer developerId1 = developerRunner.addDeveloper("Ihor", "Developer", "Java Developer", 3, 2000);
+        Integer developerId2 = developerRunner.addDeveloper("First", "Developer", "C++ Developer", 10, 2000);
+        Integer developerId3 = developerRunner.addDeveloper("Second", "Developer", "C# Developer", 5, 2000);
+        Integer developerId4 = developerRunner.addDeveloper("Third", "Developer", "PHP Developer", 1, 2000);
+
+        System.out.println("List of Developers with experience 3 years:");
+        developerRunner.listDevelopersOverThreeYears();
+
+        sessionFactory.close();
+    }
+
+    public Integer addDeveloper(String firstName, String lastName, String specialty, int experience, int salary) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
+        Integer developerId = null;
+
+        transaction = session.beginTransaction();
+        Developer developer = new Developer(firstName, lastName, specialty, experience, salary);
+        developerId = (Integer) session.save(developer);
+        transaction.commit();
+        session.close();
+        return developerId;
+    }
+
+    public void listDevelopersOverThreeYears() {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
+
+        transaction = session.beginTransaction();
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<Developer> cr = cb.createQuery(Developer.class);
+        Root<Developer> root = cr.from(Developer.class);
+        cr.select(root).where(cb.equal(root.get("experience"), 3));
+        List<Developer> dev = session.createQuery(cr).getResultList();
+
+        for (Developer developer : dev) {
+            System.out.println("=======================");
+            System.out.println(developer);
+            System.out.println("=======================");
+        }
+        transaction.commit();
+        session.close();
+    }
+
+    public void totalSalary() {
+        Session session  = sessionFactory.openSession();
+        Transaction transaction = null;
+
+        transaction = session.beginTransaction();
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<Developer> cr = cb.createQuery(Developer.class);
+        Root<Developer> root = cr.from(Developer.class);
+        //cr.select(cb.sum(root.get("salary"))); //Я рили хз... Почему он тут ругается...
+        Query<Developer> q = session.createQuery(cr);
+        List totalSalary = q.getResultList();
+        System.out.println("Total salary of all developers: " + totalSalary.get(0));
+        transaction.commit();
+        session.close();
+    }
+}
+
